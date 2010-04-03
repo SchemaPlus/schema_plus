f572c627a44e69c80aa63e608ae082f41d08d39c
Michał Łomnicki <michal.lomnicki@gmail.com>
2010-04-03 19:47:35 +0200

    add_column method documented

--- automatic_foreign_key/lib/automatic_foreign_key/active_record/migration.rb
+++ automatic_foreign_key/lib/automatic_foreign_key/active_record/migration.rb
@@ -5,6 +5,20 @@ module AutomaticForeignKey::ActiveRecord
     end
 
     module ClassMethods
+      # Overrides standard ActiveRecord add column and adds 
+      # foreign key if column references other column
+      #
+      # add_column('comments', 'post_id', :integer)
+      #   # creates a column and adds foreign key on posts(id)
+      #
+      # add_column('addresses', 'citizen_id', :integer, :references => :users
+      #   # creates a column and adds foreign key on users(id)
+      #
+      # add_column('addresses', 'citizen_id', :integer, :references => [:users, :uuid]
+      #   # creates a column and adds foreign key on users(uuid)
+      #
+      # add_column('users', 'verified')
+      #   # just creates a column as usually
       def add_column(table_name, column_name, type, options = {})
         super
         references = ActiveRecord::Base.references(table_name, column_name, options)
