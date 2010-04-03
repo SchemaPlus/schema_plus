7fc42de170a7caff34380f6f0a1c3356e1ed609b
Michał Łomnicki <michal.lomnicki@gmail.com>
2010-04-03 15:10:06 +0200

    documentation for references method

--- automatic_foreign_key/lib/automatic_foreign_key/active_record/base.rb
+++ automatic_foreign_key/lib/automatic_foreign_key/active_record/base.rb
@@ -1,10 +1,24 @@
 module AutomaticForeignKey::ActiveRecord
   module Base
-    def self.included(base)
+    def self.included(base) # :nodoc
       base.extend(ClassMethods)
     end
 
     module ClassMethods
+      # Determines referenced table and column.
+      # Used in migrations.
+      #   references('comments', 'post_id') # => ['posts', 'id']
+      #
+      # If <tt>column_name</tt> is parent_id it references to the same table
+      #   references('pages', 'parent_id')  # => ['pages', 'id']
+      #
+      # If referenced table cannot be determined properly it may be overriden
+      #   references('widgets', 'main_page_id', :references => 'pages')) 
+      #   # => ['pages', 'id']
+      #
+      # Also whole result may be given by hand
+      #   references('addresses', 'member_id', :references => ['users', 'uuid'])
+      #   # => ['users', 'uuid']
       def references(table_name, column_name, options = {})
         column_name = column_name.to_s
         if options.has_key?(:references)
