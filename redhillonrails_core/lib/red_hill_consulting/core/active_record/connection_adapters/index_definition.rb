5f1e3a6aa9de6ce410d652ab9a6d6f7723e1a001
François Beausoleil <francois@teksol.info>
2010-10-18 14:32:31 -0400

    Retabbed

--- redhillonrails_core/lib/red_hill_consulting/core/active_record/connection_adapters/index_definition.rb
+++ redhillonrails_core/lib/red_hill_consulting/core/active_record/connection_adapters/index_definition.rb
@@ -1,19 +1,19 @@
 module RedHillConsulting::Core::ActiveRecord::ConnectionAdapters
-	module IndexDefinition
-		def case_sensitive?
-			@case_sensitive.nil? ? true : @case_sensitive
-		end
+  module IndexDefinition
+    def case_sensitive?
+      @case_sensitive.nil? ? true : @case_sensitive
+    end
 
-		def case_sensitive=(case_sensitive)
-			@case_sensitive = case_sensitive
-		end
+    def case_sensitive=(case_sensitive)
+      @case_sensitive = case_sensitive
+    end
 
-		def conditions=(conditions)
-			@conditions = conditions
-		end
+    def conditions=(conditions)
+      @conditions = conditions
+    end
 
-		def conditions
-			@conditions
-		end
-	end
+    def conditions
+      @conditions
+    end
+  end
 end
