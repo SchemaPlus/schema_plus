b62f49c4103a466af5f47ef900ba0bd47240f0d9
François Beausoleil <francois@teksol.info>
2010-10-18 14:33:00 -0400

    Simplification

--- redhillonrails_core/lib/red_hill_consulting/core/active_record/connection_adapters/index_definition.rb
+++ redhillonrails_core/lib/red_hill_consulting/core/active_record/connection_adapters/index_definition.rb
@@ -1,19 +1,13 @@
 module RedHillConsulting::Core::ActiveRecord::ConnectionAdapters
   module IndexDefinition
+    attr_accessor :conditions
+
     def case_sensitive?
       @case_sensitive.nil? ? true : @case_sensitive
     end
 
     def case_sensitive=(case_sensitive)
       @case_sensitive = case_sensitive
-    end
-
-    def conditions=(conditions)
-      @conditions = conditions
-    end
-
-    def conditions
-      @conditions
     end
   end
 end
