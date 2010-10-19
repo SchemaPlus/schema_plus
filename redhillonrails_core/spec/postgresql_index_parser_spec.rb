a73e8abb67cec8d1c8ca5b4cca4acd86b2e6b235
François Beausoleil <francois@teksol.info>
2010-10-19 11:41:47 -0400

    More tests are good

--- redhillonrails_core/spec/postgresql_index_parser_spec.rb
+++ redhillonrails_core/spec/postgresql_index_parser_spec.rb
@@ -33,7 +33,48 @@ describe RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::PostgresqlAd
 
     index = indexes.first
     index.name.should == "index_users_on_username"
+    index.columns.should == ["username"]
     index.unique.should == false
+    index.should be_case_sensitive
+    index.expression.should be_nil
+  end
+
+end
+
+describe RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::PostgresqlAdapter, "unique indexes" do
+
+  before :all do
+    @migrator = Class.new(ActiveRecord::Migration) do
+      def self.up
+        create_table :users do |t|
+          t.string :username
+        end
+
+        add_index :users, :username, :unique => true
+      end
+
+      def self.down
+        drop_table :users
+      end
+    end
+  end
+
+  before do
+    @migrator.up
+  end
+
+  after do
+    @migrator.down
+  end
+
+  it "should parse the index and return appropriate information" do
+    indexes = User.indexes
+    indexes.length.should == 1
+
+    index = indexes.first
+    index.name.should == "index_users_on_username"
+    index.columns.should == ["username"]
+    index.unique.should == true
     index.should be_case_sensitive
     index.expression.should be_nil
   end
