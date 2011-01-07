d758ebe2df2126cbe036acfe7687beab390854ec
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-01-08 00:32:12 +0100

    load schema in rspec initializer

--- redhillonrails_core/spec/spec_helper.rb
+++ redhillonrails_core/spec/spec_helper.rb
@@ -1,14 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
+require 'active_record'
 require 'redhillonrails_core'
 require 'connection'
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 
 Spec::Runner.configure do |config|
   config.include(RedhillonrailsCoreMatchers)
+  # load schema
+  ActiveRecord::Migration.suppress_messages do
+    eval(File.read(File.join(File.dirname(__FILE__), 'schema', 'schema.rb')))
+  end
 end
 
-def load_schema
-  eval(File.read(File.join(File.dirname(__FILE__), 'schema', 'schema.rb')))
-end
