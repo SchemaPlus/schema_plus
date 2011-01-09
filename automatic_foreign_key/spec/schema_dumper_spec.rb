bc0a4601b0ecfd473bea54d4b6f952b7e313c31c
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-01-09 19:01:03 +0100

    added example for schema dumper

---
+++ automatic_foreign_key/spec/schema_dumper_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'stringio'
+
+require 'models/post'
+
+describe "Schema dump" do
+
+  let(:dump) do
+    stream = StringIO.new
+    ActiveRecord::SchemaDumper.ignore_tables = []
+    ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
+    stream.string
+  end
+
+  it "shouldn't include :index option for index" do
+    add_column(:author_id, :integer, :references => :users, :index => true) do
+      dump.should_not match(/index => true/)
+    end
+  end
+    
+  protected
+  def add_column(column_name, *args)
+    table = Post.table_name
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.add_column(table, column_name, *args)
+      Post.reset_column_information
+      yield if block_given?
+      ActiveRecord::Migration.remove_column(table, column_name)
+    end
+  end
+
+end
