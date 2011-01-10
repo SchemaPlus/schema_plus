bd208a9d61a2ac0d1a5cf461d4a9dcc4b57ffb27
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-01-10 02:26:08 +0100

    fixed reference matcher

--- automatic_foreign_key/spec/support/matchers/reference.rb
+++ automatic_foreign_key/spec/support/matchers/reference.rb
@@ -14,7 +14,7 @@ module AutomaticForeignKeyMatchers
       if @references_table_name
         @result = @model.foreign_keys.select do |fk|
           fk.references_table_name == @references_table_name && 
-            fk.references_column_names == @references_column_names
+            @references_column_names.empty? ? true : fk.references_column_names == @references_column_names 
         end
       else
         @result = @model.foreign_keys
@@ -26,7 +26,7 @@ module AutomaticForeignKeyMatchers
             (@on_delete ? fk.on_delete == @on_delete : true)
         end
       else
-        !!@result
+        !@result.empty?
       end
     end
 
