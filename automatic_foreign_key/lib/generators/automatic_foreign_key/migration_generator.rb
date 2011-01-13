e7a888e38abfdde41b97afe2ef5b20a676a95b00
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-01-13 03:05:22 +0100

    enclose migration generator with Generators module

--- automatic_foreign_key/lib/generators/automatic_foreign_key/migration_generator.rb
+++ automatic_foreign_key/lib/generators/automatic_foreign_key/migration_generator.rb
@@ -2,35 +2,37 @@ require 'rails/generators'
 require 'rails/generators/active_record'
 
 module AutomaticForeignKey
-  class MigrationGenerator < ::ActiveRecord::Generators::Base
-    argument :name, :default => 'create_foreign_keys'
+  module Generators
+    class MigrationGenerator < ::ActiveRecord::Generators::Base
+      argument :name, :default => 'create_foreign_keys'
 
-    def self.source_root
-      File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
-    end
-
-    def create_migration_file
-      set_local_assigns!
-      migration_template 'migration.rb', "db/migrate/#{file_name}"
-    end
-
-    protected
-    attr_reader :foreign_keys
-
-    def set_local_assigns!
-      @foreign_keys = determine_foreign_keys
-    end
-
-    def determine_foreign_keys
-      foreign_keys = []
-      connection = ::ActiveRecord::Base.connection
-      connection.tables.each do |table_name|
-        connection.columns(table_name).each do |column|
-          references = ::ActiveRecord::Base.references(table_name, column.name)
-          foreign_keys << ::RedhillonrailsCore::ActiveRecord::ConnectionAdapters::ForeignKeyDefinition.new(nil, table_name, column.name, references.first, references.last) if references
-        end
+      def self.source_root
+        File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
       end
-      foreign_keys
+
+      def create_migration_file
+        set_local_assigns!
+        migration_template 'migration.rb', "db/migrate/#{file_name}"
+      end
+
+      protected
+      attr_reader :foreign_keys
+
+      def set_local_assigns!
+        @foreign_keys = determine_foreign_keys
+      end
+
+      def determine_foreign_keys
+        foreign_keys = []
+        connection = ::ActiveRecord::Base.connection
+        connection.tables.each do |table_name|
+          connection.columns(table_name).each do |column|
+            references = ::ActiveRecord::Base.references(table_name, column.name)
+            foreign_keys << ::RedhillonrailsCore::ActiveRecord::ConnectionAdapters::ForeignKeyDefinition.new(nil, table_name, column.name, references.first, references.last) if references
+          end
+        end
+        foreign_keys
+      end
     end
   end
 end
