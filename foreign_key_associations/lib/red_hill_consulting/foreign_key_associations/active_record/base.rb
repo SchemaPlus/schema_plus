44831f45e3460f580f19e562b1b90ad2c8010c1c
Ronen Barzel <barzel@adobe.com>
2011-02-01 14:03:27 -0800

    More robust association names.
    + basic case:
    	e.g. table 'post' has column 'author' that's a foreign key of table 'authors'.
    		post.belongs_to :author
    		author.has_many :posts
    
    + If either table name is the prefix or suffix of the other, it's removed in the association name.
       e.g. 'post' has column 'post_author_id'	that's a foreign key of table 'post_authors'
    		post.belongs_to :author
    		post_author.has_many :posts
    
    	or 	'author_posts' has column 'author_id'	that's a foreign key of table 'authors'
    		author_post.belongs_to :author
    		author.has_many :posts
    
    + If the column name is a prefixed or suffixed version of the foreign table name, that prefix or suffix is propogated to the assocciation names:
    
    	e.g. 'post' has two columns 'primary_author_id' and 'secondary_author_id' that are both foreign keys of table 'authors'.
    		post.belongs_to :primary_author
    		post.belongs_to :secondary_author
    		author.has_many :posts_as_primary
    		author.has_many :posts_as_secondary
    
    + If the column name does not include the foreign table name, the column name is propogated as is to the association names:
    	e.g. 'post' has three columns 'primary_author_id', 'secondary_author_id' and 'editor_id' that are  foreign keys of table 'users'.
    		post.belongs_to :primary_author
    		post.belongs_to :secondary_author
    		post.belongs_to :editor
    		user.has_many :posts_as_primary_author
    		user.has_many :posts_as_secondary_author
    		user.has_many :posts_as_editor
    
    (of course, if the foreign key index is unique, 'has_many' will be 'has_one' and the names will be singular)

--- foreign_key_associations/lib/red_hill_consulting/foreign_key_associations/active_record/base.rb
+++ foreign_key_associations/lib/red_hill_consulting/foreign_key_associations/active_record/base.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 module RedHillConsulting::ForeignKeyAssociations::ActiveRecord
   module Base
     def self.included(base)
@@ -41,63 +43,105 @@ module RedHillConsulting::ForeignKeyAssociations::ActiveRecord
         @foreign_key_associations_loaded = true
 
         reverse_foreign_keys.each do | foreign_key |
-          next unless foreign_key.column_names.size == 1
-
-          column_name = foreign_key.column_names.first
-          next unless column_name =~ /^(.*)_id$/
-
-          begin
-            referencing_class = compute_type(foreign_key.table_name.classify)
-          rescue NameError
-            case foreign_key.table_name
-            when /^#{table_name}_(.*)$/, /^(.*)_#{table_name}$/
-              referencing_table_name = $1
-              referencing_class_name = referencing_table_name.classify
-              referencing_class = compute_type(referencing_class_name)
-
-              association_id = referencing_class.name.demodulize.underscore
-              association_id = $1 if association_id =~ /^#{self.name.underscore.singularize}_(.*)$/
-              association_id = association_id.pluralize.to_sym
-
-              has_and_belongs_to_many association_id, :class_name => referencing_class.name, :join_table => foreign_key.table_name unless method_defined?(association_id)
-            else
-              raise
+          if foreign_key.table_name =~ /^#{table_name}_(.*)$/ || foreign_key.table_name =~ /^(.*)_#{table_name}$/
+            if info = _association_info(foreign_key, $1)
+              has_and_belongs_to_many info.has_many, :class_name => info.referencing_class.name, :join_table => foreign_key.table_name unless method_defined?(info.has_many)
             end
+          else
+            _define_association(foreign_key)
           end
-
-          referencing_class.load_foreign_key_associations
         end
 
         foreign_keys.each do | foreign_key |
-          next unless foreign_key.column_names.size == 1
+          _define_association(foreign_key)
+        end
+      end
 
-          column_name = foreign_key.column_names.first
-          next unless column_name =~ /^(.*)_id$/
+      private
 
-          columns = columns_hash
-          column = columns[column_name]
+      def _define_association(fk)
+          if info = _association_info(fk)
+            columns = info.referencing_class.columns_hash
+            column = columns[info.column_name]
 
-          association_id = $1.to_sym
-          references_class_name = foreign_key.references_table_name.classify
-          references_class = compute_type(references_class_name)
+            column_name = fk.column_names.first
 
-          # belongs_to
-          belongs_to association_id, :class_name => references_class_name, :foreign_key => column_name unless method_defined?(association_id)
+            # belongs_to
+            info.referencing_class.belongs_to info.belongs_to, :class_name => info.references_class.name, :foreign_key => info.column_name unless info.referencing_class.method_defined?(info.belongs_to) and info.belongs_to != :type
 
-          # has_one/has_many
-          association_id = self.name.demodulize.underscore
-          association_id = $1 if association_id =~ /^#{references_class_name.underscore.singularize}_(.*)$/
+            # has_one/has_many
+            options = { :class_name => info.referencing_class.name, :foreign_key => info.column_name }
+            if column.unique? && column.unique_scope.empty?
+              info.references_class.has_one(info.has_one, options) unless info.references_class.method_defined?(info.has_one) and info.has_one != :type
+            else
+              options[:order] = :position if columns.has_key?('position')
+              info.references_class.has_many(info.has_many, options) unless info.references_class.method_defined?(info.has_many)
+            end
+            info.referencing_class.load_foreign_key_associations
+            info.references_class.load_foreign_key_associations
+          end
+      end
 
-          options = { :class_name => name, :foreign_key => column_name }
-          if column.unique? && column.unique_scope.empty?
-            association_id = association_id.to_sym
-            references_class.has_one(association_id, options) unless references_class.method_defined?(association_id)
-          else
-            options[:order] = :position if columns.has_key?('position')
-            association_id = association_id.pluralize.to_sym
-            references_class.has_many(association_id, options) unless references_class.method_defined?(association_id)
+
+      def _association_info(fk, referencing_table_name=nil)
+        return nil unless fk.column_names.size == 1
+
+        column_name = fk.column_names.first
+        association_name = column_name.sub(/_id$/, '')
+        references_name = fk.references_table_name.singularize
+        referencing_name = (referencing_table_name || fk.table_name).singularize.underscore
+
+        references_concise = _strip_name(references_name, referencing_name)
+        referencing_concise = _strip_name(referencing_name, references_name)
+
+        references_class = compute_type(references_name.classify) rescue nil
+        referencing_class = compute_type(referencing_name.classify) rescue nil
+
+        return nil unless references_class && referencing_class
+
+        case association_name
+        when references_name
+          belongs_to = references_concise
+          has_one = referencing_concise
+          has_many = referencing_concise.pluralize
+        when /(.*)_#{references_name}$/, /(.*)_#{references_concise}$/
+          label = $1
+          belongs_to = "#{label}_#{references_concise}"
+          has_one = "#{referencing_concise}_as_#{label}"
+          has_many = "#{referencing_concise.pluralize}_as_#{label}"
+        when /^#{references_name}_(.*)$/, /^#{references_concise}_(.*)$/
+          label = $1
+          belongs_to = "#{references_concise}_#{label}"
+          has_one = "#{referencing_concise}_as_#{label}"
+          has_many = "#{referencing_concise.pluralize}_as_#{label}"
+        else
+          belongs_to = association_name
+          has_one = "#{referencing_concise}_as_#{association_name}"
+          has_many = "#{referencing_concise.pluralize}_as_#{association_name}"
+        end
+
+        OpenStruct.new(
+          :column_name => column_name,
+          :belongs_to => belongs_to.to_sym,
+          :has_one => has_one.to_sym,
+          :has_many => has_many.to_sym,
+          :references_name => references_name,
+          :references_class => references_class,
+          :referencing_name => referencing_name,
+          :referencing_class => referencing_class
+        )
+      end
+
+      def _strip_name(string, *names)
+        names.each do |name|
+          name = name.underscore.singularize
+          if string =~ /^#{name}_(.*)$/
+            string = $1
+          elsif string =~ /(.*)_#{name}$/
+            string = $1
           end
         end
+        string
       end
     end
   end
