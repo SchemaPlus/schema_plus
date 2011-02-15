8f40dc3f41c9c5635674150019e5a03678a7b550
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-02-15 03:40:50 +0100

    use config.foreign_keys in install generator template

--- lib/generators/active_schema/templates/active_schema.rb
+++ lib/generators/active_schema/templates/active_schema.rb
@@ -1,16 +1,34 @@
 ActiveSchema.setup do |config|
 
+  # Migration will generate foreign key if column name has _id suffix.
+  # config.foreign_keys.auto_create = true
+  #
+  # Examples:
+  #
+  # when a column is added foreign_key will be generated automagically
+  # add_column :posts, :user_id # adds FK to users(id)
+  #
+  # create_table :comments do |t|
+  #   t.integer :post_id # adds FK to posts(id)
+  # end
+  #
+  # force author_id to reference users table
+  # t.integer :author_id, :references => :users
+  #
+  # prevent foreign key from being created
+  # t.string :session_id, :references => nil
+  #
   # Default ON UPDATE action
   # Available values are :cascade, :restrict, :set_null, :set_default, :no_action
-  # config.on_update = :cascade
+  # config.foreign_keys.on_update = :cascade
   #
   # Default ON DELETE action
   # Available values are :cascade, :restrict, :set_null, :set_default, :no_action
-  # config.on_delete = :restrict
+  # config.foreign_keys.on_delete = :restrict
   #
-  # Auto indexing foreign keys.
-  # It's relevant for PostgreSQL only.
+  # Create an index on foreign key column.
+  # It's relevant for PostgreSQL and SQLite only.
   # MySQL engines auto-index foreign keys by default
-  # config.auto_index = true
+  # config.foreign_keys.auto_index = true
 
 end
