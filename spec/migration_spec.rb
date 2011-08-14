2aeb86c996587112cc82dbbda7af18a00c4a28c9
ronen barzel <ronen@barzel.org>
2011-08-14 11:20:11 -0700

    test and document use of t.references

--- spec/migration_spec.rb
+++ spec/migration_spec.rb
@@ -36,6 +36,16 @@ describe ActiveRecord::Migration do
       @model.should reference(:users, :id).on(:user_id)
     end
 
+    it "should create foreign key using t.references" do
+      create_table(@model,  :user => {:METHOD => :references})
+      @model.should reference(:users, :id).on(:user_id)
+    end
+
+    it "should not create foreign key using t.references with :references => nil" do
+      create_table(@model,  :user => {:METHOD => :references, :references => nil})
+      @model.should_not reference(:users, :id).on(:user_id)
+    end
+
     it "should create foreign key to the same table on parent_id" do
       create_table(@model,  :parent_id => {})
       @model.should reference(@model.table_name, :id).on(:parent_id)
