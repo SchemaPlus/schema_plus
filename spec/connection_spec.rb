3c402fc1c9ed7cddb2fca543a86324a25fd554c6
ronen barzel <ronen@barzel.org>
2011-10-07 20:56:47 +0100

    added spec for testing re-connection (fails for mysql2)

---
+++ spec/connection_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Connection" do
+
+  it "should re-open without failure" do
+    expect {
+      ActiveRecord::Base.establish_connection "schema_plus"
+    }.should_not raise_error
+  end
+end
