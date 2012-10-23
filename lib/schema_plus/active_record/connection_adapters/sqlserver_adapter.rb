a60359346a43d46160f71a4abcee4f03f1124475
Jacob Carlborg <jacob.carlborg@tretti.se>
2012-10-23 17:01:46 +0200

    Implement "foreign_keys" and "reverse_foreign_keys" for SQL Server.

--- lib/schema_plus/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/schema_plus/active_record/connection_adapters/sqlserver_adapter.rb
@@ -2,52 +2,55 @@ module SchemaPlus
   module ActiveRecord
     module ConnectionAdapters
       module SqlserverAdapter
-        # (abstract) Returns the names of all views, as an array of strings
-        def views (name = nil) raise "Internal Error: Connection adapter didn't override abstract function"; [] end
+        ON_UPDATE = "on_update"
+        ON_DELETE = "on_delete"
 
-        # (abstract) Returns the SQL definition of a given view.  This is
-        # the literal SQL would come after 'CREATVE VIEW viewname AS ' in
-        # the SQL statement to create a view.
-        def view_definition (view_name, name = nil) raise "Internal Error: Connection adapter didn't override abstract function"; end
+        # # (abstract) Returns the names of all views, as an array of strings
+        # def views (name = nil) raise "Internal Error: Connection adapter didn't override abstract function"; [] end
+        # 
+        # # (abstract) Returns the SQL definition of a given view.  This is
+        # # the literal SQL would come after 'CREATVE VIEW viewname AS ' in
+        # # the SQL statement to create a view.
+        # def view_definition (view_name, name = nil) raise "Internal Error: Connection adapter didn't override abstract function"; end
 
         # (abstract) Return the ForeignKeyDefinition objects for foreign key
         # constraints defined on this table
         def foreign_keys (table_name, name = nil)
-          load_foreign_keys table_name, false, name
+          load_foreign_keys(table_name, false, name)
         end
 
         # (abstract) Return the ForeignKeyDefinition objects for foreign key
         # constraints defined on other tables that reference this table
         def reverse_foreign_keys (table_name, name = nil)
-          load_foreign_keys table_name, true, name
+          load_foreign_keys(table_name, true, name)
         end
 
-        # (abstract) Return true if the passed expression can be used as a column
-        # default value.  (For most databases the specific expression
-        # doesn't matter, and the adapter's function would return a
-        # constant true if default expressions are supported or false if
-        # they're not.)
-        def default_expr_valid? (expr) raise "Internal Error: Connection adapter didn't override abstract function"; end
-
-        # (abstract) Return SQL definition for a given canonical function_name symbol.
-        # Currently, the only function to support is :now, which should
-        # return a DATETIME object for the current time.
-        def sql_for_function (function_name) raise "Internal Error: Connection adapter didn't override abstract function"; end
+        # # (abstract) Return true if the passed expression can be used as a column
+        # # default value.  (For most databases the specific expression
+        # # doesn't matter, and the adapter's function would return a
+        # # constant true if default expressions are supported or false if
+        # # they're not.)
+        # def default_expr_valid? (expr) raise "Internal Error: Connection adapter didn't override abstract function"; end
+        # 
+        # # (abstract) Return SQL definition for a given canonical function_name symbol.
+        # # Currently, the only function to support is :now, which should
+        # # return a DATETIME object for the current time.
+        # def sql_for_function (function_name) raise "Internal Error: Connection adapter didn't override abstract function"; end
 
       private
 
         def load_foreign_keys (table_name, reverse, name = nil)
           table = reverse ? "KCU_REF" : "KCU_FK"
 
-          result = query(<<-SQL, name)
+          query= <<-SQL
             SELECT  
               KCU_FK.CONSTRAINT_NAME AS name,
               KCU_FK.TABLE_NAME AS table_name,
               KCU_FK.COLUMN_NAME AS column_names,
               KCU_REF.TABLE_NAME AS references_table_name,
               KCU_REF.COLUMN_NAME AS references_column_names,
-              RC.UPDATE_RULE as on_update,
-              RC.DELETE_RULE as on_delete,
+              RC.UPDATE_RULE as #{ON_UPDATE},
+              RC.DELETE_RULE as #{ON_DELETE},
               TC.IS_DEFERRABLE as deferrable
             FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS AS RC 
 
@@ -70,7 +73,15 @@ module SchemaPlus
             where #{table}.TABLE_NAME = '#{table_name}'
           SQL
 
-          result
+          exec_query(query, name).to_hash.map do |row|
+            replace_row_value(row, ON_UPDATE)
+            replace_row_value(row, ON_DELETE)
+            ForeignKeyDefinition.new(*row.values)
+          end
+        end
+
+        def replace_row_value (row, key)
+          row[key] = ForeignKeyDefinition::ACTIONS_REVERSED[row[key]]
         end
       end
     end
