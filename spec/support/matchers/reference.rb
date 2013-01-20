79ca546ee52724ef42a4a69b4212a2885d7c546d
ronen barzel <ronen@barzel.org>
2013-01-20 21:42:29 +0000

    fix references matcher to work even if column names aren't specified.  remove redundant file.

--- spec/support/matchers/reference.rb
+++ spec/support/matchers/reference.rb
@@ -19,23 +19,24 @@ module SchemaPlusMatchers
       else
         @result = @model.foreign_keys
       end
-      if @column_names 
-        @result.any? do |fk| 
-          fk.column_names == @column_names &&
-            (@on_update ? fk.on_update == @on_update : true) &&
-            (@on_delete ? fk.on_delete == @on_delete : true) &&
-            (@name ? fk.name == @name : true)
-        end
-      else
-        !@result.empty?
-      end
+      @result.keep_if {|fk| fk.column_names == @column_names } if @column_names
+      @result.keep_if {|fk| fk.on_update == @on_update } if @on_update
+      @result.keep_if {|fk| fk.on_delete == @on_delete } if @on_delete
+      @result.keep_if {|fk| fk.name == @name } if @name
+      !@result.empty?
     end
 
     def failure_message_for_should(should_not = false)
       target_column_names = @column_names.present? ? "(#{@column_names.join(', ')})" : "" 
       destinantion_column_names = @references_table_name ? "#{@references_table_name}(#{@references_column_names.join(', ')})" : "anything"
       invert = should_not ? 'not' : ''
-      "Expected #{@model.table_name}#{target_column_names} to #{invert} reference #{destinantion_column_names}"
+      msg = "Expected #{@model.table_name}#{target_column_names} to #{invert} reference #{destinantion_column_names}"
+      with = []
+      with << "on_update=#{@on_update.inspect}" if @on_update
+      with << "on_delete=#{@on_delete.inspect}" if @on_delete
+      with << "name=#{@name.inspect}" if @name
+      msg += " with #{with.join(" and ")}" if with.any?
+      msg
     end
 
     def failure_message_for_should_not
