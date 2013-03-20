33cc076bb2f1d994e57a770e2d182e37ef1aa889
ronen barzel <ronen@barzel.org>
2013-03-20 22:45:35 +0000

    allow rails 3.2.13.  tweak railtie initialization to work with it
    addresses #91.  closes #95.

--- lib/schema_plus/railtie.rb
+++ lib/schema_plus/railtie.rb
@@ -1,7 +1,7 @@
 module SchemaPlus
   class Railtie < Rails::Railtie #:nodoc:
 
-    initializer 'schema_plus.insert', :after => "active_record.initialize_database" do
+    initializer 'schema_plus.insert', :before => "active_record.initialize_database" do
       ActiveSupport.on_load(:active_record) do
         SchemaPlus.insert
       end
