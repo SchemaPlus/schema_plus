a5da6745e3fbb8961a74ad23bccbd0586d1818b4
ronen barzel <ronen@barzel.org>
2013-03-20 22:44:40 +0000

    spec fix: don't use let definition in before(:all)

--- spec/named_schemas_spec.rb
+++ spec/named_schemas_spec.rb
@@ -1,7 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "with multiple schemas" do
-  let (:connection) { ActiveRecord::Base.connection }
+  def connection
+    ActiveRecord::Base.connection
+  end
 
   before(:all) do
     newdb = case connection.adapter_name
