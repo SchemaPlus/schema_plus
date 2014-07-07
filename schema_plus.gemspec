590496874aaf684be70bd0b24e1f37b9163f480e
ronen barzel <ronen@barzel.org>
2014-07-07 14:32:06 +0100

    specs now pass for ActiveRecord >= 4.1.2
    problem was with the column default specs.  changed them to now verify the desired default value gets assigned to a record, rather than inspecting the SQL created by an internal ActiveRecord method — whose behavior changed with 4.1.2, breaking the old tests.
    
    Closes #159

--- schema_plus.gemspec
+++ schema_plus.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_dependency("activerecord", ">= 3.2", "<= 4.1.1")
+  s.add_dependency("activerecord", ">= 3.2")
   s.add_dependency("valuable")
 
   s.add_development_dependency("rake")
---
+++ spec/column_default_spec.rb
@@ -0,0 +1,150 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+
+describe "Column definition" do
+  before(:each) do
+    define_schema(:auto_create => true) do
+      create_table :models, :force => true do |t|
+      end
+    end
+    class Model < ::ActiveRecord::Base ; end
+  end
+
+  subject { Model.create.reload.test_column }
+
+  context "text columns" do
+
+    context "just default passed" do
+      before(:each) do
+        define_test_column(:string, :default => "2011-12-11 00:00:00")
+      end
+
+      it "should use the normal default" do
+        is_expected.to eq "2011-12-11 00:00:00"
+      end
+    end
+
+    context "just default passed in hash" do
+      before(:each) do
+        define_test_column(:string, :default => { :value => "2011-12-11 00:00:00" })
+      end
+
+      it "should use the normal default" do
+        is_expected.to eq "2011-12-11 00:00:00"
+      end
+    end
+
+    context "default passed with no nulls" do
+      before(:each) do
+        define_test_column(:string, :default => "2011-12-11 00:00:00", null: false)
+      end
+
+      it "should use the normal default" do
+        is_expected.to eq "2011-12-11 00:00:00"
+      end
+    end
+
+    context "default passed in hash with no nulls" do
+      before(:each) do
+        define_test_column(:string, :default => { :value => "2011-12-11 00:00:00" }, null: false)
+      end
+
+      it "should use the normal default" do
+        is_expected.to eq "2011-12-11 00:00:00"
+      end
+    end
+
+    context "default function passed as now" do
+      before(:each) do
+        begin
+          define_test_column(:string, :default => :now)
+          @nowish = /#{Time.now.utc.to_s.sub(/:[^:]+$/, '')}.*/
+        rescue ArgumentError => e
+          @raised_argument_error = e
+        end
+      end
+
+      if SchemaPlusHelpers.mysql?
+        it "should raise an error" do
+          expect(@raised_argument_error).to be_a ArgumentError
+        end
+      else
+        it "should use NOW() as the default" do
+          is_expected.to match @nowish
+        end
+      end
+    end
+
+    context "default function passed as now with no nulls" do
+      before(:each) do
+        begin
+          define_test_column(:string, :default => :now, null: false)
+          @nowish = /#{Time.now.utc.to_s.sub(/:[^:]+$/, '')}.*/
+        rescue ArgumentError => e
+          @raised_argument_error = e
+        end
+      end
+
+      if SchemaPlusHelpers.mysql?
+        it "should raise an error" do
+          expect(@raised_argument_error).to be_a ArgumentError
+        end
+      else
+        it "should use NOW() as the default" do
+          is_expected.to match @nowish
+        end
+      end
+    end
+
+    context "valid expr passed as default" do
+      if SchemaPlusHelpers.mysql?
+        it "raises an error" do
+          expect {
+            define_test_column(:string, :default => { :expr => "(replace('THIS IS A TEST', 'TEST', 'DOG'))" })
+          }.to raise_error ArgumentError
+        end
+      else
+        it "uses the expression" do
+          define_test_column(:string, :default => { :expr => "(replace('THIS IS A TEST', 'TEST', 'DOG'))" })
+          is_expected.to eq "THIS IS A DOG"
+        end
+      end
+    end
+
+  end
+
+  context "boolean column" do
+
+    context "passed as boolean false" do
+      before(:each) do
+        define_test_column :boolean, :default => false
+      end
+
+      it "should give the default as false" do
+        is_expected.to eq false
+      end
+    end
+
+    context "passed as boolean true" do
+      before(:each) do
+        define_test_column :boolean, :default => true
+      end
+
+      it "should give the default as true" do
+        is_expected.to eq true
+      end
+    end
+  end
+
+  private 
+
+  def define_test_column(type, *args)
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.create_table Model.table_name, :force => true do |t|
+        t.send type, :test_column, *args
+      end
+    end
+    Model.reset_column_information
+    @column = Model.columns.first()
+  end
+end
