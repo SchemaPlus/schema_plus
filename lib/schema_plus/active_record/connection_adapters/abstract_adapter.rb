13e6cfc4d709c14ce1a0d8974987d024fe285a9c
Roberto Miranda <rjmaltamar@gmail.com>
2014-12-22 18:57:55 -0500

    Drop view if force true

--- lib/schema_plus/active_record/connection_adapters/abstract_adapter.rb
+++ lib/schema_plus/active_record/connection_adapters/abstract_adapter.rb
@@ -49,7 +49,9 @@ module SchemaPlus
         # to first drop the view if it already exists.
         def create_view(view_name, definition, options={})
           definition = definition.to_sql if definition.respond_to? :to_sql
-          execute "DROP VIEW IF EXISTS #{quote_table_name(view_name)}" if options[:force]
+          if options[:force]
+            drop_view(view_name, if_exists: true)
+          end
           execute "CREATE VIEW #{quote_table_name(view_name)} AS #{definition}"
         end
 
