955176b437b2b215a04d7259bbd21fb4399ab7d2
ronen barzel <ronen@barzel.org>
2015-01-14 14:26:46 +0000

    conform to schema_monkey changes

--- lib/schema_plus/schema_default.rb
+++ lib/schema_plus/schema_default.rb
@@ -5,10 +5,12 @@ require_relative 'schema_default/db_default'
 require_relative 'schema_default/postgresql_exec_cache'
 
 module SchemaDefault
-  module ConnectionAdapters
-    module PostgresqlAdapter
-      def self.included(base)
-        SchemaMonkey::Middleware::ExecCache.insert(0, SchemaDefault::PostgreqlExecCache)
+  module ActiveRecord
+    module ConnectionAdapters
+      module PostgresqlAdapter
+        def self.included(base)
+          SchemaMonkey::Middleware::ExecCache.insert(0, SchemaDefault::PostgreqlExecCache)
+        end
       end
     end
   end
