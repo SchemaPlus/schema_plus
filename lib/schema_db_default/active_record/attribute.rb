4252b72edc06ca1d831acf420e080e6646bb7803
ronen barzel <ronen@barzel.org>
2015-01-15 10:33:23 +0000

    rename schema_plus => schema_db_default in alias method chains

--- lib/schema_db_default/active_record/attribute.rb
+++ lib/schema_db_default/active_record/attribute.rb
@@ -2,15 +2,15 @@ module SchemaDbDefault
   module ActiveRecord
     module Attribute
       def self.included(base)
-        base.alias_method_chain :original_value, :schema_plus
+        base.alias_method_chain :original_value, :schema_db_default
       end
 
-      def original_value_with_schema_plus
+      def original_value_with_schema_db_default
         # prevent attempts to cast DB_DEFAULT to the attributes type.
         # We want to keep it as DB_DEFAULT so that we can handle it when
         # generating the sql.
         return DB_DEFAULT if value_before_type_cast == DB_DEFAULT
-        original_value_without_schema_plus
+        original_value_without_schema_db_default
       end
 
     end
