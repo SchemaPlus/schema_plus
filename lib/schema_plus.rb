db0f9b7a9fc2acca9636d0c0b171cc98c809a681
ronen barzel <ronen@barzel.org>
2015-01-15 03:16:56 +0000

    hoist schema_default_expr

--- lib/schema_plus.rb
+++ lib/schema_plus.rb
@@ -3,6 +3,7 @@ require 'valuable'
 
 require 'schema_monkey'
 require 'schema_db_default'
+require 'schema_default_expr'
 
 require 'schema_plus/version'
 require 'schema_plus/active_record/base'
@@ -17,7 +18,6 @@ require 'schema_plus/active_record/connection_adapters/column'
 require 'schema_plus/active_record/connection_adapters/foreign_key_definition'
 require 'schema_plus/active_record/connection_adapters/index_definition'
 require 'schema_plus/active_record/migration/command_recorder'
-require 'schema_plus/schema_default_expr'
 
 module SchemaPlus
   module ActiveRecord
