9059a145606788d07000a121f048fa5e6d466a5d
ronen barzel <ronen@barzel.org>
2015-01-15 10:24:54 +0000

    conform middleware to schema_monkey convention

--- lib/schema_db_default.rb
+++ lib/schema_db_default.rb
@@ -2,20 +2,9 @@ require 'schema_monkey'
 
 require_relative 'schema_db_default/active_record/attribute'
 require_relative 'schema_db_default/db_default'
+require_relative 'schema_db_default/middleware'
 
 module SchemaDbDefault
-
-  autoload :PostgreqlExecCache, 'schema_db_default/postgresql_exec_cache'
-
-  module ActiveRecord
-    module ConnectionAdapters
-      module PostgresqlAdapter
-        def self.included(base)
-          SchemaMonkey::Middleware::ExecCache.insert(0, SchemaDbDefault::PostgreqlExecCache)
-        end
-      end
-    end
-  end
 
   def self.insert
     ::ActiveRecord.const_set(:DB_DEFAULT, SchemaDbDefault::DB_DEFAULT)
---
+++ lib/schema_db_default/middleware.rb
@@ -0,0 +1,30 @@
+module SchemaDbDefault
+  module Middleware
+    module PostgresqlAdapter
+      def self.insert
+        SchemaMonkey::Middleware::ExecCache.insert(0, BindDbDefault)
+      end
+    end
+
+    # Middleware to replace each ActiveRecord::DB_DEFAULT with a literal
+    # DEFAULT in the sql string, for postgresql.  The underlying pg gem provides no
+    # way to bind a value that will replace $n with DEFAULT.
+    class BindDbDefault < SchemaMonkey::Middleware::Base
+      def call(env)
+        if env.binds.any?{ |col, val| val.equal? ::ActiveRecord::DB_DEFAULT}
+          j = 0
+          env.binds.each_with_index do |(col, val), i|
+            if val.equal? ::ActiveRecord::DB_DEFAULT
+              env.sql = env.sql.sub(/\$#{i+1}/, 'DEFAULT')
+            else
+              env.sql = env.sql.sub(/\$#{i+1}/, "$#{j+1}") if i != j
+              j += 1
+            end
+          end
+          env.binds = env.binds.reject{|col, val| val.equal? ::ActiveRecord::DB_DEFAULT}
+        end
+        @app.call(env)
+      end
+    end
+  end
+end
