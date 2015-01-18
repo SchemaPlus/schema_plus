30a83dd5dddf772329d08114092ce7b5f376dae0
ronen barzel <ronen@barzel.org>
2015-01-18 01:02:10 +0000

    use middleware for sqlite3 enhancements

--- lib/schema_index_plus.rb
+++ lib/schema_index_plus.rb
@@ -2,6 +2,7 @@ require 'schema_monkey'
 
 require_relative 'schema_index_plus/middleware'
 require_relative 'schema_index_plus/middleware/postgresql'
+require_relative 'schema_index_plus/middleware/sqlite3'
 require_relative 'schema_index_plus/active_record/connection_adapters/abstract_adapter'
 require_relative 'schema_index_plus/active_record/connection_adapters/column'
 require_relative 'schema_index_plus/active_record/connection_adapters/index_definition'
@@ -11,7 +12,6 @@ module SchemaIndexPlus
   module ActiveRecord
     module ConnectionAdapters
       autoload :MysqlAdapter, 'schema_index_plus/active_record/connection_adapters/mysql_adapter'
-      autoload :Sqlite3Adapter, 'schema_index_plus/active_record/connection_adapters/sqlite3_adapter'
     end
   end
 
---
+++ lib/schema_index_plus/middleware/sqlite3.rb
@@ -0,0 +1,25 @@
+module SchemaIndexPlus
+  module Middleware
+    module Sqlite3
+      def self.insert
+        SchemaMonkey::Middleware::Query::Indexes.append LookupExtensions
+      end
+
+      class LookupExtensions < SchemaMonkey::Middleware::Base
+        def call(env)
+          continue env
+          indexes = Hash[env.index_definitions.map{ |d| [d.name, d] }]
+
+          env.connection.exec_query("SELECT name, sql FROM sqlite_master WHERE type = 'index'").map do |row|
+            if (desc_columns = row['sql'].scan(/['"`]?(\w+)['"`]? DESC\b/).flatten).any?
+              index = indexes[row['name']]
+              index.orders = Hash[index.columns.map {|column| [column, desc_columns.include?(column) ? :desc : :asc]}]
+            end
+          end
+
+          env.index_definitions
+        end
+      end
+    end
+  end
+end
