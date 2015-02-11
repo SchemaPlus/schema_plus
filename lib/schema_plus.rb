464a987b04ef039ef49a369ff1e95f9f3bd9ab16
ronen barzel <ronen@barzel.org>
2015-02-11 23:08:30 +0000

    schema_plus_indexes -> schema_plus/indexes

--- lib/schema_plus.rb
+++ lib/schema_plus.rb
@@ -4,10 +4,10 @@ require 'schema_plus/db_default'
 require 'schema_plus/default_expr'
 require 'schema_plus/enums'
 require 'schema_plus/foreign_keys'
-require 'schema_plus_indexes'
-require 'schema_plus_pg_indexes'
+require 'schema_plus/indexes'
 require 'schema_plus/tables'
 require 'schema_plus/views'
+require 'schema_plus_pg_indexes'
 
 require_relative 'schema_plus/version'
 
